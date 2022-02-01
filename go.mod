2c0eb920229e197db12cbaf310b0158e4993d9f2
Sobhan Ebrahimi <ebrahimi@hesaba.co>
2022-02-01 19:28:16 +0330

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module TweetFetcher
+
+go 1.13
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/antchfx/htmlquery v1.2.4 // indirect
+	github.com/antchfx/xmlquery v1.3.9 // indirect
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/n0madic/twitter-scraper v0.0.0-20220124081915-396358bd501b
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+)
---
+++ main/main.go
@@ -0,0 +1,86 @@
+package main
+
+//goland:noinspection ALL
+import (
+	"fmt"
+	colly "github.com/gocolly/colly/v2"
+	"log"
+	"strconv"
+)
+
+type Proxy struct {
+	IP   string
+	port uint16
+}
+
+func extractProxies() []Proxy {
+	var proxies []Proxy
+
+	collyCollector := colly.NewCollector(
+		colly.Async(),
+		colly.CacheDir("./proxy_list_cache"),
+	)
+
+	err := collyCollector.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 4})
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	collyCollector.OnHTML("table[class=\"table table-striped table-bordered\"] tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, row *colly.HTMLElement) {
+			proxy := Proxy{}
+			row.ForEach("td", func(_ int, el *colly.HTMLElement) {
+				switch el.Index {
+				case 0:
+					proxy.IP = el.Text
+				case 1:
+					var port, err = strconv.Atoi(el.Text)
+					if err != nil {
+						log.Fatal(err)
+					}
+					proxy.port = uint16(port)
+				}
+			})
+			proxies = append(proxies, proxy)
+		})
+	})
+
+	err2 := collyCollector.Visit("https://free-proxy-list.net/")
+	if err2 != nil {
+		log.Fatal(err2)
+		return nil
+	}
+	collyCollector.Wait()
+
+	return proxies
+}
+
+func main() {
+	var proxies = extractProxies()
+
+	fmt.Println(len(proxies))
+	/*
+		scraper := twitterscraper.New()
+		scraper.SetSearchMode(twitterscraper.SearchLatest)
+
+		var userTweets []twitterscraper.Tweet
+		for tweet := range scraper.GetTweets(context.Background(), "Twitter", 10) {
+			if tweet.Error != nil {
+				panic(tweet.Error)
+			}
+			userTweets = append(userTweets, tweet.Tweet)
+		}
+
+		jsonString, err := json.Marshal(userTweets)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = ioutil.WriteFile("big_marhsall.json", jsonString, os.ModePerm)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	*/
+}
