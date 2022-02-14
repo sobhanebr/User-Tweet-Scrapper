be82ab068cd8001ceb823a141a168a6da045d64d
Sobhan Ebrahimi <ebrahimi@hesaba.co>
2022-02-14 11:18:56 +0330

    reader was completed

--- main/main.go
+++ main/main.go
@@ -3,26 +3,55 @@ package main
 //goland:noinspection ALL
 import (
 	"context"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	colly "github.com/gocolly/colly/v2"
 	twitterscraper "github.com/n0madic/twitter-scraper"
 	"github.com/vijayviji/executor"
-
-	//"github.com/vijayviji/executor"
 	"io/ioutil"
 	"log"
 	"os"
 	"strconv"
 )
 
-const maxConcurrency = 10
+const MaxConcurrency uint32 = 50
+const MaxTweetsPerUser = 100
+const UserProcessPerProxy uint32 = 100
+const OutputPath string = "tweets"
 
-var throttle = make(chan int, maxConcurrency)
+var counter uint32 = 0
+var currentProxyIndex = 0
+
+var proxies = extractProxies()
 
 type Proxy struct {
-	IP   string
-	port uint16
+	IP    string
+	port  uint16
+	https bool
+}
+
+type User struct {
+	Id         uint64
+	screenName string
+}
+
+func createUserList(data [][]string) []User {
+	var userList []User
+	for i, line := range data {
+		if i > 0 { // skip header line
+			var rec User
+			for j, field := range line {
+				if j == 0 {
+					rec.Id, _ = strconv.ParseUint(field, 10, 64)
+				} else if j == 1 {
+					rec.screenName = field
+				}
+			}
+			userList = append(userList, rec)
+		}
+	}
+	return userList
 }
 
 func extractProxies() []Proxy {
@@ -54,6 +83,8 @@ func extractProxies() []Proxy {
 						log.Fatal(err)
 					}
 					proxy.port = uint16(port)
+				case 6:
+					proxy.https = el.Text == "yes"
 				}
 			})
 			proxies = append(proxies, proxy)
@@ -70,50 +101,107 @@ func extractProxies() []Proxy {
 	return proxies
 }
 
-func tokhs() {
-	ex := executor.NewFixedThreadPool("executorName", 10, 2000)
-	dataForTask := "Dummy data"
+func spreadTasks(userList []User) (future []executor.Future) {
+	var futures []executor.Future
+	createOutputDir()
+	twitterscraper.SetSearchMode(twitterscraper.SearchLatest)
+	ex := executor.NewFixedThreadPool("executorName", MaxConcurrency, 2000)
+	for _, user := range userList {
+		userFileName := fmt.Sprintf("%s/%d.json", OutputPath, user.Id)
+		if _, err := os.Stat(userFileName); err == nil {
+			log.Println(userFileName + " already exists, skipping")
+			continue
+		}
+		var future = ex.Submit(func(taskData interface{}, threadName string, taskID uint64) interface{} {
+			taskUser := taskData.(User)
+			if counter == 0 {
+				counter = UserProcessPerProxy
+				err := twitterscraper.SetProxy(enrollProxy())
+				if err != nil {
+					log.Fatal(err)
+				}
+				counter--
+			}
+			var userTweets []twitterscraper.Tweet
+			for tweet := range twitterscraper.WithDelay(5).GetTweets(context.Background(), taskUser.screenName, MaxTweetsPerUser) {
+				if tweet.Error != nil {
+					log.Println(tweet.Error)
+				}
+				userTweets = append(userTweets, tweet.Tweet)
+			}
 
-	future := ex.Submit(func(taskData interface{}, threadName string, taskID uint64) interface{} {
-		dataFromTask := taskData.(string)
-		fmt.Println("data for this task: ", dataFromTask)
-		return "OKKK"
-	}, dataForTask)
+			jsonString, err := json.Marshal(userTweets)
+			if err != nil {
+				log.Println(err)
+			}
+			err = ioutil.WriteFile(userFileName, jsonString, os.ModePerm)
+			if err != nil {
+				log.Fatal(err)
+			}
+			return fmt.Sprintf("Tweets of user with ID %d was wrote in %s", user.Id, userFileName)
+		}, user)
+		futures = append(futures, future)
+	}
+	return
+}
 
-	result := future.Get()
-	fmt.Println(result)
-	// result will be "OKKK"
+func enrollProxy() string {
+	if currentProxyIndex == len(proxies) {
+		currentProxyIndex = 0
+	}
+	var proxy = proxies[currentProxyIndex]
+	currentProxyIndex++
+	var prefix string
+	if proxy.https {
+		prefix = "https"
+	} else {
+		prefix = "http"
+	}
+	return fmt.Sprintf("%s://%s:%d", prefix, proxy.IP, proxy.port)
+}
 
-	// taskStatus can be any of executor.TaskNotStarted, executor.TaskStarted, executor.TaskDone
-	taskStatus := future.GetStatus()
-	fmt.Println(taskStatus)
+func createOutputDir() {
+	if _, err := os.Stat(OutputPath); os.IsNotExist(err) {
+		err := os.Mkdir(OutputPath, 0775)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func main() {
-	var proxies = extractProxies()
 
-	fmt.Println(len(proxies))
+	// load users
+	userList := loadUserList()
+	futures := spreadTasks(userList)
 
-	scraper := twitterscraper.New()
-	scraper.SetSearchMode(twitterscraper.SearchLatest)
+	for _, future := range futures {
+		result := future.Get()
+		log.Println(result)
+	}
+}
 
-	var userTweets []twitterscraper.Tweet
-	for tweet := range scraper.GetTweets(context.Background(), "Twitter", 10) {
-		if tweet.Error != nil {
-			panic(tweet.Error)
+func loadUserList() []User {
+	file, err := os.Open("nonLocatedUserDetails.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// close the file at the end of the program
+	defer func(f *os.File) {
+		err := f.Close()
+		if err != nil {
+			log.Fatal(err)
 		}
-		userTweets = append(userTweets, tweet.Tweet)
+	}(file)
+
+	// read csv values using csv.Reader
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	jsonString, err := json.Marshal(userTweets)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = ioutil.WriteFile("big_marhsall.json", jsonString, os.ModePerm)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
+	// convert records to array of structs
+	return createUserList(data)
 }
